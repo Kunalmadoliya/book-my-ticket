bbad767a11867cc5f884e4836374c78838532f4c
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-04-15 14:41:46 +0530

    feat: update cookie settings for refresh token and enhance login/register forms in index.html

--- backend/src/module/auth/auth.controller.ts
+++ backend/src/module/auth/auth.controller.ts
@@ -15,7 +15,7 @@ const login = async (req: Request, res: Response) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
